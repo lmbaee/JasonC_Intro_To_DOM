a03b8154acbcdd6cdbd44d8e3bd6e32243b0060d
Jason Cardenas <zombiekiler1221@gmail.com>
2025-04-05 11:55:13 -0700

    Used document.querySelector() to select and log three different elements (h1, p, and button)

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
-let titleElement = document.getElementById("title");
+let heading = document.querySelector("#main-heading");
+let paragraph = document.querySelector(".description");
+let button = document.querySelector("#changeText");
 
-console.log(titleElement);
+console.log(heading);
+console.log(paragraph);
+console.log(button);
